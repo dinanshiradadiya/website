2205cadba6a98580ae06fc3d8b93ddf3ab9c6e15
paraliyavishal <paraliyavishu007@gmail.com>
2024-07-19 09:35:27 +0530

    vishalfirst commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import Home from './pages/Home'
-import Product1 from './pages/Product1'
-import Blogs from './pages/Blogs'
-import Collection from './pages/Collection'
-import { Routes, Route} from 'react-router-dom'
+import Product from './component/Product'
+import Productprops from './component/Productprops'
+import Productdescreption from './component/Productdescreption'
+import Reletedproduct from './component/Reletedproduct'
 
 function App() {
   return (
     <div>
-      {/* <Home /> */}
-      {/* <Product1 /> */}
-        {/* <Collection /> */}
-      {/* <Blogs /> */}
-      {/* <Footer /> */}
-
-      <Routes>
-        <Route path='/' element={<Home />}></Route>
-        <Route path='/product1' element={<Product1 />}></Route>
-        <Route path='/collection' element={<Collection />}></Route>
-        <Route path='/blogs' element={<Blogs />}></Route>
-      </Routes>
+      {/* <Product /> */}
+      {/* <Productprops /> */}
+      <Productdescreption />
+      {/* <Reletedproduct /> */}
     </div>
   )
 }
