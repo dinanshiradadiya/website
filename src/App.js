48a425387fc2eba6711e624f92757945f728932e
ankurvaghasiya24 <avaghasiya544@gmail.com>
2024-07-19 16:23:43 +0530

    ankur no changes

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import Product from './component/Product'
 import Productprops from './component/Productprops'
 import Productdescreption from './component/Productdescreption'
 import Reletedproduct from './component/Reletedproduct'
-<<<<<<< HEAD
 import { Route, Routes } from 'react-router-dom'
 import Product1 from './pages/Product1'
 import Home from "./pages/Home"
 import Collections  from './pages/Collection'
 import Blogs from "./pages/Blogs"
-=======
->>>>>>> 137abb62df29591a6c284ba0e802d9d407f2c094
 
 function App() {
   return (
--- src/pages/Product1.js
+++ src/pages/Product1.js
@@ -1,7 +1,10 @@
 import React from 'react'
-import Product from '../components/product/Product'
 import Header from '../commoncomponents/Header'
+import Product from '../component/Product'
 import Footer from '../commoncomponents/Footer'
+// import Product from '../components/product/Product'
+// import Header from '../commoncomponents/Header'
+// import Footer from '../commoncomponents/Footer'
 
 function Product1() {
   return (
